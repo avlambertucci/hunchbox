3e5a65d30b4003133a98936614db061a651b4cee
avlambertucci <armandolambertucci@gmail.com>
2020-11-01 13:45:55 -0300

    key base64 deploy

--- pages/api/get-promo.js
+++ pages/api/get-promo.js
@@ -3,9 +3,12 @@ import {GoogleSpreadsheet} from 'google-spreadsheet';
 
 const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID)
 
- 
+ const fromBase64 = (value)=>{
+   const buff = Buffer.from(value, 'base64');
+   return buff.toString('ascii');
+ }
  export default async (req, res)=>{
-  
+  // console.log(fromBase64(process.env.SHEET_PRIVATE_KEY))
   try{
     // await doc.useServiceAccountAuth(credentials)
     await doc.useServiceAccountAuth({
