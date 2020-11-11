6c080479c8ee349c099ec9b173efb4dcbe9a8a0b
avlambertucci <armandolambertucci@gmail.com>
2020-11-11 19:06:20 -0300

    final

--- components/Map/Map.js
+++ components/Map/Map.js
@@ -4,7 +4,7 @@ import 'leaflet/dist/leaflet.css'
 
 const Map = () => {
 
-  
+  const access_key = 'pk.eyJ1IjoiYXZsYW1iZXJ0dWNjaSIsImEiOiJja2diM201Ym4wZGRwMnlwcXczZHZscjdpIn0.OQ88znlUuqw8WQFe-IZJ-w'
   return (
     <div className="p-4">
       <MapContainer 
@@ -14,7 +14,7 @@ const Map = () => {
       style={{height: 400, width: 700}}
       >
         <TileLayer
-          url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.NEXT_PUBLIC_MAPBOX_TOKEN2}`}
+          url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${access_key}`}
           
         />
         
